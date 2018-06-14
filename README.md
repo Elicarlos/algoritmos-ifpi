14024936b75636478ff0bc83f293067d6e29e569
Elicarlos <elicarlosantos_@hotmail.com>
2018-06-14 20:15:37 -0300

    meu primeiro commit

--- README.md
+++
@@ -1,1 +0,0 @@
-"# algoritomos-ifpi" 
--- helloword.py
+++
@@ -1,8 +0,0 @@
-print('Ola boa noite, tudo bem? Qual o seu nome')
-nome = input()
-print('Caraca! {} que nome legal! mas parece nome de menina kk' .format(nome))
-
-print('{} ! me falaram que você é um bom programador(a), é verdade? '.format(nome))
-resposta = input()
-
-print('Falar que {}, parece babaquice kk ' . format(resposta))
